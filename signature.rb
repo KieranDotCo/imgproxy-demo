de001bf30f861da82c19c42d30d98a8f1ece10db
Kieran Whiteman <kieran@kieran.co>
2024-01-31 11:28:30 +0000

    task: Signature

--- signature.rb
+++ signature.rb
@@ -7,10 +7,11 @@ key = ["dca562cbe2b96cc3bd79162fd5b0122f7394e3593ef8a1447da8959448fa4075"].pack(
 salt = ["cd4842017110a9412ddaa36d13dce193edd76a2ed4f46e91855570003468b148"].pack("H*")
 
 url = ARGV[0]
+processing_opts = ARGV[1] || ''
 
 encoded_url = Base64.urlsafe_encode64(url).tr("=", "").scan(/.{1,16}/).join("/")
 
-path = "/#{encoded_url}"
+path = "#{processing_opts.nil? ? '' : '/' + processing_opts}/#{encoded_url}"
 
 digest = OpenSSL::Digest.new("sha256")
 hmac = Base64.urlsafe_encode64(OpenSSL::HMAC.digest(digest, key, "#{salt}#{path}")).tr("=", "")
